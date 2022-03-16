8b8083afa0b58902e6c0fee7411413efc3dbc915
biancanilsen <biancanilsen.entra21@gmail.com>
2022-03-16 16:42:12 -0300

    cadastro projeto de alocação

--- src/app/modules/projects/projects.module.ts
+++ src/app/modules/projects/projects.module.ts
@@ -9,9 +9,17 @@ import {MatButtonModule} from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table'  
 import { ProjectsResourcesTabComponent } from './projects-create/projects-resources-tab/projects-resources-tab.component';
 import { ProjectsRegisterTabComponent } from './projects-create/projects-register-tab/projects-register-tab.component';
-import { ProjectsPanelTabComponent } from './projects-create/projects-panel-tab/projects-panel-tab.component';
+import { ProjectsPanelTabComponent } from './projects-create/projects-register-tab/projects-panel-tab/projects-panel-tab.component';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { PortalComponent } from './portal/portal/portal.component';
 
 const routes: Routes = [
   {
@@ -26,6 +34,7 @@ const routes: Routes = [
     ProjectsResourcesTabComponent,
     ProjectsRegisterTabComponent,
     ProjectsPanelTabComponent,
+    PortalComponent,
   ],
   imports: [
     RouterModule.forChild(routes), 
@@ -36,6 +45,13 @@ const routes: Routes = [
     ReactiveFormsModule,
     MatTableModule,
     MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaskModule,
+    MatCheckboxModule,
   ],
   entryComponents: [
     ProjectsCreateComponent,
