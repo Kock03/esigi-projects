3423d163c5d8b9f2026157eefe54bd961bafb67a
biancanilsen <biancanilsen.entra21@gmail.com>
2022-03-21 15:30:11 -0300

    panel

--- src/app/modules/projects/projects-create/projects-panel-tab/projects-panel-tab.component.ts
+++ src/app/modules/projects/projects-create/projects-panel-tab/projects-panel-tab.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
 
+
+export interface Panel {
+  ressource: string;
+  hours: string;
+  status: string;
+}
 @Component({
   selector: 'app-projects-panel-tab',
   templateUrl: './projects-panel-tab.component.html',
   styleUrls: ['./projects-panel-tab.component.scss']
 })
 export class ProjectsPanelTabComponent implements OnInit {
+  @ViewChild('projectTable') projectTable!: MatTable<any>;
+
+  displayedPanel: string[] = [
+    'ressource',
+    'hours',
+    'status',
+  ];
+
+  panels: Panel[] = [
+    {
+      ressource: 'João da Silva',
+      hours: '170',
+      status: 'ativo',
+    },
+  ];
+
+  filteredProjectPanelList!: any[];
 
   constructor() { }
 
