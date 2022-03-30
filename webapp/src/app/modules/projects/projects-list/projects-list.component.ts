0f2d0fd0b705ff82b6805af1315f7465586172dc
biancanilsen <biancanilsen.entra21@gmail.com>
2022-03-30 15:20:14 -0300

    busca de projetos

--- webapp/src/app/modules/projects/projects-list/projects-list.component.ts
+++ webapp/src/app/modules/projects/projects-list/projects-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, fromEvent } from 'rxjs';
 import { IProjects } from 'src/app/interfaces/iproject';
 import { ProjectProvider } from 'src/providers/project.provider';
 
@@ -11,6 +13,10 @@ import { ProjectProvider } from 'src/providers/project.provider';
   styleUrls: ['./projects-list.component.scss'],
 })
 export class ProjectsListComponent implements OnInit {
+  @ViewChild(MatSort) sort: MatSort = new MatSort();
+  @ViewChild('filter', { static: true }) filter!: ElementRef;
+
+
   displayedProjects: string[] = [
     'type',
     'name',
@@ -31,6 +37,40 @@ export class ProjectsListComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.getProjectList();
+    this.initFilter();
+  }
+
+  initFilter() {
+    fromEvent(this.filter.nativeElement, 'keyup')
+      .pipe(debounceTime(200), distinctUntilChanged())
+
+      .subscribe((res) => {
+        this.filteredProjectList.data = this.projects.filter(
+          (project) =>
+          project.name
+              .toLocaleLowerCase()
+              .includes(this.filter.nativeElement.value.toLocaleLowerCase())
+        );
+      });
+  }
+
+  async selectList(ev: any) {
+    if (ev.value == 1) {
+      return (this.filteredProjectList = this.projects =
+        await this.projectsProvider.findAll());
+    }
+    // if (ev.value == 2) {
+    //   return (this.filteredProjectList = this.projects =
+    //     await this.projectsProvider.findActive());
+    // }
+    // if (ev.value == 3) {
+    //   return (this.filteredProjectList = this.projects =
+    //     await this.projectsProvider.findInactive());
+    // }
+  }
+
+  editCollaborator(collaboratorId: any) {
+    this.router.navigate([`colaborador/${collaboratorId}`]);
   }
 
   createProject() {
@@ -45,5 +85,6 @@ export class ProjectsListComponent implements OnInit {
   async editProject(projectSelected: any) {
     this.project = projectSelected;
     sessionStorage.setItem('project_type', this.project.type.toString());
+    this.router.navigate(['projeto/alocacao:id']);
   }
 }
