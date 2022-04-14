3b0af3e22b9b0d6f84d7123eedcb26692e1d4a79
biancanilsen <biancanilsen.entra21@gmail.com>
2022-04-14 16:26:22 -0300

    mask datePicker

--- webapp/src/app/modules/projects/projects-create/projects-register-tab/projects-register-tab.component.ts
+++ webapp/src/app/modules/projects/projects-create/projects-register-tab/projects-register-tab.component.ts
@@ -1,18 +1,47 @@
+import { formatDate } from '@angular/common';
 import {
   Component,
   EventEmitter,
+  Injectable,
   Input,
   OnInit,
   Output,
   ViewEncapsulation,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DateAdapter, MAT_DATE_FORMATS, NativeDateAdapter } from '@angular/material/core';
 import { ActivatedRoute } from '@angular/router';
+
+
+export const PICK_FORMATS = {
+  parse: { dateInput: { month: 'numeric', year: 'numeric', day: 'numeric' } },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'numeric' },
+    dateA11yLabel: { year: 'numeric', month: 'numeric', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'numeric' },
+  },
+};
+
+@Injectable()
+export class PickDateAdapter extends NativeDateAdapter {
+  override format(date: Date, displayFormat: Object): string {
+    if (displayFormat === 'input') {
+      return formatDate(date, 'dd-MM-yyyy', this.locale);
+    } else {
+      return date.toDateString();
+    }
+  }
+}
 
 @Component({
   selector: 'app-projects-register-tab',
   templateUrl: './projects-register-tab.component.html',
   styleUrls: ['./projects-register-tab.component.scss'],
+  providers: [
+    { provide: DateAdapter, useClass: PickDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: PICK_FORMATS },
+  ],
   encapsulation: ViewEncapsulation.None,
 })
 export class ProjectsRegisterTabComponent implements OnInit {
